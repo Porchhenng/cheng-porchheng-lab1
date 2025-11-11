1077c25823811c56c0c1fc5a9495de01a94b54d9
Porchheng <porchheng914@gmail.com>
2025-11-11 15:41:11 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 // const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, GitHub Actions!');
+    res.send('TESTING WORK FLOW TRIGGER');
 });
 
 export default app;
